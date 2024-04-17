68b8cfe235cb9c2407a64dbd827361df1cc304b3
yutelkova <yutelkova@phoenixit.ru>
2024-04-17 10:38:14 +0300

    add algorithm for graph and lenght

--- algorithm.py
+++ algorithm.py
@@ -20,3 +20,16 @@ class Graph(object):
 
         return graph
 
+    def get_nodes(self):
+        return self.nodes
+
+    def get_outgoing_edges(self, node):
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        return self.graph[node1][node2]
+
